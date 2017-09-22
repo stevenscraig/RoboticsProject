12cd2795dbd9b6c2c1348dcf6e7ba896bc0e1f0c
stevenscraig <31775914+stevenscraig@users.noreply.github.com>
2017-09-22 08:46:37 -0400

    Wall Follow Test

--- WallFollowTest.c
+++ WallFollowTest.c
@@ -6,27 +6,23 @@
 
 task main()
 {
-	while(SensorValue(button)==0)
+	while(true)
 	{
 		motor[Right] =100; motor[Left] =100;
-		wait1Msec(1000);
+		wait1Msec(100);
+
+		if(SensorValue(button)==1)
+		{
+			motor[Right] =-100; motor[Left]= -100;
+			wait1Msec(250);
+			motor[Right]=0; motor[Left] = -100;
+			wait1Msec(500);
+		 		while(SensorValue(button)==0)
+  			{
+					motor[Right]=60; motor[Left] =100;
+  				wait1Msec(100);
+				}
+		}
+
 	}
-
-	motor[Right] =50; motor[Left]= -50;
-wait1Msec(500);
-
-while(SensorValue(button)==0)
-{
-	motor[Right] =75; motor[Left] =100;
-	wait1Msec(1000);
-
-	if(SensorValue(button)==1)
-	{
-		motor[Right] =-100; motor[Left]= -100;
-		wait1Msec(3000);
-		motor[Right]=0; motor[Left] = -100;
-		wait1Msec(750);
-	}
-}
-
 }
